8e3f811d0126f3552ce89e6ed647c1592fee586a
jawadchy2150 <jawadchowdhury260@gmail.com>
2025-03-04 23:26:19 +0600

    done with get and post request

---
+++ 18-Flask/flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return render_template('index.html')
+
+@app.route("/form", methods= ['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f"hello {name}"
+    return render_template('form.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
